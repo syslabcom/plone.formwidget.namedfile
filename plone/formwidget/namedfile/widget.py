976395274a6a9e7202a4363c37de3644ec602366
JMehring <JMehring@localhost>
2010-07-20 02:27:04 +0000

    namedfile fixes
    svn path=/plone.formwidget.namedfile/branches/jmehring-drafts/; revision=38160

--- plone/formwidget/namedfile/widget.py
+++ plone/formwidget/namedfile/widget.py
@@ -33,10 +33,14 @@ class NamedFileWidget(Explicit, file.FileWidget):
     
     @property
     def allow_nochange(self):
-        return not self.ignoreContext and \
-                   self.field is not None and \
-                   self.value is not None and \
-                   self.value != self.field.missing_value
+        #return not self.ignoreContext and \
+        #           self.field is not None and \
+        #           self.value is not None and \
+        #           self.value != self.field.missing_value
+        allow_nochange = self.field is not None and \
+                         self.value is not None and \
+                         self.value != self.field.missing_value
+        return allow_nochange
     
     @property
     def filename(self):
@@ -80,16 +84,21 @@ class NamedFileWidget(Explicit, file.FileWidget):
     
     def extract(self, default=NOVALUE):
         action = self.request.get("%s.action" % self.name, None)
+        #self.request["%s.action" % self.name] = 'nochange' #DEBUG
+        #self.request["%s.action" % self.name] = '' #DEBUG
+        
         if action == 'remove':
             return None
-        elif action == 'nochange':
+        
+        value = super(NamedFileWidget, self).extract(default)
+        if action == 'nochange' and value == NOVALUE:
             if self.form.ignoreContext:
                 return default
             dm = getMultiAdapter((self.context, self.field,), IDataManager)
             return dm.get()
 
         # empty unnamed FileUploads should not count as a value
-        value = super(NamedFileWidget, self).extract(default)
+        #value = super(NamedFileWidget, self).extract(default)
         if isinstance(value, FileUpload):
             value.seek(0, SEEK_END)
             empty = value.tell()==0
